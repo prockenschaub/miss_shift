55eb7a8e2198cfb897669ebf8066063535bd91e8
prockenschaub <rockenschaub.patrick@gmail.com>
2023-05-02 13:43:34 +0200

    only one plot for mar shift

--- python/plot_shift.py
+++ python/plot_shift.py
@@ -7,7 +7,7 @@ scores = [load_scores(e) for e in experiments]
 scores = [find_best_params(s) for s in scores]
 scores = [diff_to_bayes(s, var) for s in scores]
 
-plot_all(scores, var, type='box', n=2e4)
+plot_one(scores[0], var, type='box')
 
 
 # Experiment where X_obs stays the same at test time
@@ -17,4 +17,4 @@ scores = [load_scores(e) for e in experiments]
 scores = [find_best_params(s) for s in scores]
 scores = [diff_to_bayes(s, var) for s in scores]
 
-plot_all(scores, var, type='box', n=2e4)
+plot_one(scores[0], var, type='box')
