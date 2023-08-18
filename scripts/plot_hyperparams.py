3cb26a866b712f4c69af73b85241a5b0178b4a0f
prockenschaub <rockenschaub.patrick@gmail.com>
2023-08-18 15:05:39 +0200

    more hyperparam inspection

--- scripts/plot_hyperparams.py
+++ scripts/plot_hyperparams.py
@@ -1,10 +1,11 @@
 from plot_utils import *
 import seaborn as sns
 
-experiment = ['main_experiment']
+experiment = ['less_miss']
+link = ['stairs']
 scenario = ['mcar']
-var = 'R2_test_m'
-scores = [load_scores(e, s) for e, s in zip(experiment, scenario)]
+var = 'mse_test_m'
+scores = [load_scores(e, l, s) for e, l, s in zip(experiment, link, scenario)]
 scores = [perf_by_params(s) for s in scores]
 
 data = scores[0]
@@ -14,16 +15,26 @@ def plot_hyperparams(method, n, param):
     sns.scatterplot(
         data[(data.method == method) & (data.n == n)],
         x=param, 
-        y='R2_val',
+        y='mse_val',
         hue='prop_latent'
     )
 
-plot_hyperparams('neumiss', 2e4, 'mlp_depth')
-plot_hyperparams('neumiss', 2e4, 'width_factor')
-plot_hyperparams('neumiss', 2e4, 'lr')
-plot_hyperparams('neumiss', 2e4, 'weight_decay')
+plot_hyperparams('mice_impute_mask', 1e5, 'mlp_depth')
+plot_hyperparams('mice_impute_mask', 1e5, 'width_factor')
+plot_hyperparams('mice_impute_mask', 1e5, 'lr')
+plot_hyperparams('mice_impute_mask', 1e5, 'weight_decay')
 
-plot_hyperparams('neumice', 2e4, 'mlp_depth')
-plot_hyperparams('neumice', 2e4, 'width_factor')
-plot_hyperparams('neumice', 2e4, 'lr')
-plot_hyperparams('neumice', 2e4, 'weight_decay')
+plot_hyperparams('multimice_impute', 1e5, 'mlp_depth')
+plot_hyperparams('multimice_impute', 1e5, 'width_factor')
+plot_hyperparams('multimice_impute', 1e5, 'lr')
+plot_hyperparams('multimice_impute', 1e5, 'weight_decay')
+
+plot_hyperparams('neumiss', 1e5, 'mlp_depth')
+plot_hyperparams('neumiss', 1e5, 'width_factor')
+plot_hyperparams('neumiss', 1e5, 'lr')
+plot_hyperparams('neumiss', 1e5, 'weight_decay')
+
+plot_hyperparams('neumice', 1e5, 'mlp_depth')
+plot_hyperparams('neumice', 1e5, 'width_factor')
+plot_hyperparams('neumice', 1e5, 'lr')
+plot_hyperparams('neumice', 1e5, 'weight_decay')
